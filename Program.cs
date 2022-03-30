bb7e79614c4da9260096959b2eb9920c5b29b9b5
Jana Bojarova <thejana13@gmail.com>
2022-03-30 18:50:07 +0300

    firstcommit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AgeCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your birthdate");
+            int yearOfBirth = Int16.Parse(Console.ReadLine());
+            int result = 2022 - yearOfBirth;
+            if (result < 18)
+            {
+                Console.WriteLine("too young to drive");
+            }
+            else if (result > 18)
+            {
+                Console.WriteLine("drive carefully");
+            }
+            else if (result == 18)
+            {
+                Console.WriteLine("Congratulations, you may apply for your driving lisence now");
+            }
+        }
+    }
+}
